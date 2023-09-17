f3045d243040bd035d9f75fa485c02704e46a1e4
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-17 03:38:30 +0000

    updated test file

--- test_app.rb
+++ test_app.rb
@@ -1,5 +1,5 @@
-# require "sinatra"
-# require "sinatra/reloader"
+require "sinatra"
+require "sinatra/reloader"
 require "http"
 require "json"
  
@@ -7,7 +7,7 @@ require "json"
  # Fetch Exchange rate symbols API URL
  exch_symbols_url = "https://api.exchangerate.host/symbols"
 
- # Place GET request to the exchange rate API url
+ # Place GET request to the exchange symbol API url
  raw_exch_symbols = HTTP.get(exch_symbols_url)
 
  # Parse the HTTP Response with JSON 
@@ -19,3 +19,24 @@ require "json"
  symbol_hash.each do |key, value|
   pp key
  end
+
+ get("/:currency_1/:currency_2") do
+  # Parse currency_1 value
+  @curr_1 = params.fetch("currency_1").upcase
+  @curr_2 = params.fetch("currency_2").upcase
+  
+  # Assemble exchange rate URL
+  exch_rate_url = "https://api.exchangerate.host/convert?from=" + @curr_1 + "&to=" + @curr_2
+
+  # Place GET request to exchange rate API
+  raw_exch_rate = HTTP.get(exch_rate_url)
+
+
+
+ 
+
+ 
+   
+  erb(:convert_2)
+
+end 
