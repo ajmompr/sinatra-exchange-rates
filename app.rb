5240d02352bfa29f3117b89cf0ad88eff898ef4c
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-17 01:20:36 +0000

    added require http & json; also added erb home method to home route

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  erb(:home)
 end
