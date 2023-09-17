f9179d34287ab46600e502ebf7fa3c89c102a4f0
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-17 03:30:22 +0000

    removed incorrect HTTP request from curr_1/curr_2 path

--- app.rb
+++ app.rb
@@ -36,22 +36,12 @@ get("/:currency_1") do
 
   # Parse currency_1 value
   @curr_1 = params.fetch("currency_1").upcase
-  
+
   erb(:convert_1)
 end
 
 get("/:currency_1/:currency_2") do
-   # Fetch Exchange rate symbols API URL
-   exch_symbols_url = "https://api.exchangerate.host/symbols"
-
-   # Place GET request to the exchange rate API url
-   raw_exch_symbols = HTTP.get(exch_symbols_url)
- 
-   # Parse the HTTP Response with JSON 
-   parsed_response_symbols = JSON.parse(raw_exch_symbols)
- 
-   # Create symbol hash to obtain currency names
-   @symbol_hash = parsed_response_symbols.fetch("symbols")
+   
  
    # Parse currency_1 value
    @curr_1 = params.fetch("currency_1").upcase
