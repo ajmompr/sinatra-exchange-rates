b45f39685109b108484576e7cd73535531cb1941
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-17 03:38:18 +0000

    assemble exch_rate_url

--- app.rb
+++ app.rb
@@ -41,11 +41,16 @@ get("/:currency_1") do
 end
 
 get("/:currency_1/:currency_2") do
-   
+  # Parse currency_1 value
+  @curr_1 = params.fetch("currency_1").upcase
+  @curr_2 = params.fetch("currency_2").upcase
+  
+  # Assemble exchange rate URL
+  exch_rate_url = "https://api.exchangerate.host/convert?from=" + @curr_1 + "&to=" + @curr_2
+  pp exch_rate_url
+
  
-   # Parse currency_1 value
-   @curr_1 = params.fetch("currency_1").upcase
-   @curr_2 = params.fetch("currency_2").upcase
+   
   erb(:convert_2)
 
 end 
